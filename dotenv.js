96265ddcf82acf7f3cd25ea014cde68dd0e8772d
Matt <carlotta.matt@gmail.com>
2020-10-27 16:08:13 -0700

    Update performance tests

--- dotenv.js
+++ dotenv.js
@@ -2,9 +2,9 @@ const dotenv = require("dotenv");
 const dotenvExpand = require("dotenv-expand");
 
 console.time("Test");
-for (let i = 0; i < 1000; i += 1) {
-  const myEnv = dotenv.config();
-  parsed = dotenvExpand(myEnv).parsed;
+for (let i = 0; i < 5000; i += 1) {
+  const myEnv = dotenv.config({ path: ".env.interp" });
+  dotenvExpand(myEnv);
   console.log("iteration", i + 1);
 }
 console.timeEnd("Test");
--- snackables.js
+++ snackables.js
@@ -1,5 +1,6 @@
 const snackables = require("snackables");
 
+// Loading NextJS ENVs
 console.time("Test");
 for (let i = 0; i < 500000; i += 1) {
   snackables.config({
@@ -8,3 +9,11 @@ for (let i = 0; i < 500000; i += 1) {
   console.log("iteration", i + 1);
 }
 console.timeEnd("Test");
+
+// Loading interpolated ".env" like dotenv
+// console.time("Test");
+// for (let i = 0; i < 5000; i += 1) {
+//   snackables.config({ path: ".env.interp" });
+//   console.log("iteration", i + 1);
+// }
+// console.timeEnd("Test");
