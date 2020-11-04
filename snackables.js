ef474288aab479b5813410472f5069c669ff596e
Matt <carlotta.matt@gmail.com>
2020-11-04 15:09:17 -0800

    Update snackables

--- snackables.js
+++ snackables.js
@@ -1,20 +1,20 @@
 const snackables = require("snackables");
 
 // loading default ".env" like dotenv
-console.time("Test");
-for (let i = 0; i < 500000; i += 1) {
-  snackables.config();
-  console.log("iteration", i + 1);
-}
-console.timeEnd("Test");
-
-// loading interpolated ".env" like dotenv+dotenv-expand
 // console.time("Test");
-// for (let i = 0; i < 5000; i += 1) {
-//   snackables.config({ path: ".env.interp" });
+// for (let i = 0; i < 500000; i += 1) {
+//   snackables.config();
 //   console.log("iteration", i + 1);
 // }
 // console.timeEnd("Test");
+
+// loading interpolated ".env" like dotenv+dotenv-expand
+console.time("Test");
+for (let i = 0; i < 5000; i += 1) {
+  snackables.config({ path: ".env.interp" });
+  console.log("iteration", i + 1);
+}
+console.timeEnd("Test");
 
 // loading default next env files (.env, .env.development, .env.local, .env.development.local)
 // console.time("Test");
