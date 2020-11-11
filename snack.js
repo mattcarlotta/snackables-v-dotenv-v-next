d19f0cd24103c4daf33130ba17ee44f6fbf73f86
Matt <carlotta.matt@gmail.com>
2020-11-11 14:16:33 -0800

    Fix broken snack tests

--- snack.js
+++ snack.js
@@ -1,15 +1,15 @@
 const snackables = require("snackables");
 
 // loading default ".env" like dotenv
-// console.time("Test");
-// for (let i = 0; i < 500000; i += 1) {
-//   snackables.config();
-//   console.log("iteration", i + 1);
-// }
-// console.timeEnd("Test");
+console.time("Test");
+for (let i = 0; i < 500000; i += 1) {
+  snackables.config();
+  console.log("iteration", i + 1);
+}
+console.timeEnd("Test");
 
 // loading interpolated ".env" like dotenv+dotenv-expand
-console.time("Test");
+// console.time("Test");
 // for (let i = 0; i < 5000; i += 1) {
 //   snackables.config({ path: [".env.interp"] });
 //   console.log("iteration", i + 1);
@@ -17,11 +17,11 @@ console.time("Test");
 // console.timeEnd("Test");
 
 // loading default next env files (.env, .env.development, .env.local, .env.development.local)
-console.time("Test");
-for (let i = 0; i < 500000; i += 1) {
-  snackables.config({
-    path: [".env", ".env.development", ".env.local", ".env.development.local"]
-  });
-  console.log("iteration", i + 1);
-}
-console.timeEnd("Test");
+// console.time("Test");
+// for (let i = 0; i < 500000; i += 1) {
+//   snackables.config({
+//     path: [".env", ".env.development", ".env.local", ".env.development.local"]
+//   });
+//   console.log("iteration", i + 1);
+// }
+// console.timeEnd("Test");
