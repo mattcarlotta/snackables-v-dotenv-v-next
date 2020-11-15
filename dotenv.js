fc8c44b83ae033a0485e59ecd7008fa549b4a3a6
Matt <carlotta.matt@gmail.com>
2020-11-14 16:12:06 -0800

    Add multi env dotenv test

--- dotenv.js
+++ dotenv.js
@@ -18,3 +18,14 @@ console.timeEnd("Test");
 //   console.log("iteration", i + 1);
 // }
 // console.timeEnd("Test");
+
+// loading default next env files (.env, .env.development, .env.local, .env.development.local)
+// console.time("Test");
+// for (let i = 0; i < 500000; i += 1) {
+//   dotenvExpand(dotenv.config({ path: ".env" }));
+//   dotenvExpand(dotenv.config({ path: ".env.development" }));
+//   dotenvExpand(dotenv.config({ path: ".env.local" }));
+//   dotenvExpand(dotenv.config({ path: ".env.development.local" }));
+//   console.log("iteration", i + 1);
+// }
+// console.timeEnd("Test");
