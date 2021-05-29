d953795b10e3130c103db774927fb262a5bbaa5f
Matt <carlotta.matt@gmail.com>
2021-05-29 09:43:04 -0700

    Fix next test run

--- next.js
+++ next.js
@@ -2,10 +2,10 @@ const { loadEnvConfig } = require("./next-src");
 
 console.time("Test");
 // loading a default .env
-// for (let i = 0; i < 500000; i += 1) {
-//   loadEnvConfig(".", [".env"]);
-//   console.log("iteration", i + 1);
-// }
+for (let i = 0; i < 500000; i += 1) {
+  loadEnvConfig(".", [".env"]);
+  console.log("iteration", i + 1);
+}
 
 // loading a large interpolated env loading
 // for (let i = 0; i < 5000; i += 1) {
@@ -14,9 +14,9 @@ console.time("Test");
 // }
 
 // loading default next env files (.env, .env.development, .env.local, .env.development.local)
-for (let i = 0; i < 500000; i += 1) {
-  loadEnvConfig();
-  console.log("iteration", i + 1);
-}
+// for (let i = 0; i < 500000; i += 1) {
+//   loadEnvConfig();
+//   console.log("iteration", i + 1);
+// }
 
 console.timeEnd("Test");
